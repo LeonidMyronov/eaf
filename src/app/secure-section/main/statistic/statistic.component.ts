0a51eed917e7c2f8ae72d9130408202fcb99dcc3
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-04-16 18:14:41 +0300

    add main components

---
+++ src/app/secure-section/main/statistic/statistic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eaf-statistic',
+  templateUrl: './statistic.component.html',
+  styleUrls: ['./statistic.component.sass']
+})
+export class StatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
