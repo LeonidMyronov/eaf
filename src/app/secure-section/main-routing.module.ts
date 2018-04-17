09d97de5dca7f83a02950e9335f9ff5aa83d5067
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-04-17 11:28:19 +0300

    add correct routing for main components

--- src/app/secure-section/main-routing.module.ts
+++ src/app/secure-section/main-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainComponent } from './main/main.component';
 import { TodayComponent } from './main/today/today.component';
 import { StatisticComponent } from './main/statistic/statistic.component';
 
 const routes: Routes = [
-  {path: '', component: TodayComponent},
-  {path: 'statistic', component: StatisticComponent}
+  {path: '', component: MainComponent, children: [
+    {path: 'today', component: TodayComponent},
+    {path: 'statistic', component: StatisticComponent},
+    {path: '**', redirectTo: 'today'},
+  ]},
 ];
 
 @NgModule({
