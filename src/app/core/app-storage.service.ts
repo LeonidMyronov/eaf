1aba59288d7f0d229f7a63f885841832d69bf43c
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-04-17 11:27:26 +0300

    add user-menu

--- src/app/core/app-storage.service.ts
+++ src/app/core/app-storage.service.ts
@@ -10,7 +10,7 @@ export class AppStorageService {
     {
       name: 'contacts',
       url: '/contacts'
-    }
+    },
   ];
 
   private langsList = [
@@ -76,40 +76,53 @@ export class AppStorageService {
 
   private userMenu = [
     {
+      name: 'promo',
+      iconSrc: '',
+    },
+    {
       name: 'today',
       iconSrc: '/assets/images/main/user-menu/icon-today.svg',
+      url: 'main/today'
     },
     {
       name: 'statistic',
       iconSrc: '/assets/images/main/user-menu/icon-statistic.svg',
+      url: 'main/statistic'
     },
     {
       name: 'post back',
       iconSrc: '/assets/images/main/user-menu/icon-postback.svg',
+      url: 'main/postback'
     },
     {
       name: 'balance',
       iconSrc: '/assets/images/main/user-menu/icon-balance.svg',
+      url: 'main/balance'
     },
     {
       name: 'whiteLabel',
       iconSrc: '/assets/images/main/user-menu/icon-whitelabel.svg',
+      url: 'main/whitelabel'
     },
     {
       name: 'offer',
       iconSrc: '/assets/images/main/user-menu/icon-offer.svg',
+      url: 'main/offer'
     },
     {
       name: 'guide',
       iconSrc: '/assets/images/main/user-menu/icon-guide.svg',
+      url: 'main/guide'
     },
     {
       name: 'news',
       iconSrc: '/assets/images/main/user-menu/icon-news.svg',
+      url: 'main/news'
     },
     {
       name: 'support',
       iconSrc: '/assets/images/main/user-menu/icon-support.svg',
+      url: 'main/support'
     },
   ];
   private homeSection1Cards = [
@@ -206,4 +219,5 @@ export class AppStorageService {
   getHomeSection1Cards() { return [...this.homeSection1Cards]; }
   getWorkTypes() { return [...this.workTypes]; }
   getHomeSection5Cards() { return [...this.homeSection5Cards]; }
+  getUserMenu() {return [...this.userMenu]; }
 }
