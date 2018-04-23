2ae89340cbe4aea4bd074f5e119f404a5fe46a8e
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-04-23 10:13:57 +0300

    add dropdowns to footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/map';
 
 import { AppStorageService } from '../core/app-storage.service';
 
@@ -16,7 +18,13 @@ export class FooterComponent implements OnInit {
   public deployPath = environment.deployPath;
   public navMenu: any;
   public userMenu: any;
+  public timeList: any;
+  public currentTime: number ;
   public isAuth$: Observable<boolean>;
+  private timerGenerator$: Observable<Date>;
+  private currentTimer$: Observable<string>;
+  private homeTimer$: Observable<string>;
+  private serverTimer$: Observable<string>;
 
   constructor(
     private appStorage: AppStorageService,
@@ -27,6 +35,26 @@ export class FooterComponent implements OnInit {
     this.isAuth$ = this.store.select(fromRoot.getIsAuth);
     this.navMenu = this.appStorage.getNavMenu();
     this.userMenu = this.appStorage.getUserMenu();
+    this.timeList = this.appStorage.getTimeList();
+    this.currentTime = this.timeList[0];
+
+    this.timerGenerator$ = this.createTimer().map(_ => new Date());
+    this.homeTimer$ = this.timerGenerator$.map(date => this.formatTime(date.getHours()) + ':' + this.formatTime(date.getMinutes()));
+    this.serverTimer$ = this.timerGenerator$.map(date => `${this.formatTime(date.getUTCHours())}:${this.formatTime(date.getMinutes())}`);
+    this.currentTimer$ = this.serverTimer$;
   }
 
+
+  createTimer() {
+    return Observable.timer(0, 60000);
+  }
+
+  formatTime(n: number) {
+    return n < 10 ? '0' + n : ''  + n;
+  }
+
+  onChangeTime(time: any) {
+    this.currentTime = time;
+    this.currentTimer$ = time.id === 1 ? this.serverTimer$ : this.homeTimer$;
+  }
 }
