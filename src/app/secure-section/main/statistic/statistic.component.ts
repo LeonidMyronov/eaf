12a9f0f68620f74e1590ea89daec0d731b9e7d4e
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-02 13:31:03 +0300

    switch routing to stat.page

--- src/app/secure-section/main/statistic/statistic.component.ts
+++ src/app/secure-section/main/statistic/statistic.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
+import * as fromRoot from '../../../app.reducers';
+import { User } from '../../user/user.model';
 @Component({
   selector: 'eaf-statistic',
   templateUrl: './statistic.component.html',
   styleUrls: ['./statistic.component.sass']
 })
 export class StatisticComponent implements OnInit {
+  public userState$: Observable<{user: User}>;
 
-  constructor() { }
+  constructor(
+    private store: Store<fromRoot.State>,
+  ) { }
 
   ngOnInit() {
+    this.userState$ = this.store.select(fromRoot.getUserState);
   }
 
 }
