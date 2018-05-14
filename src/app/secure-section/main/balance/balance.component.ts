9990805e729e468d393af5a4df23190567544383
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-14 21:47:50 +0300

    mockup balance card-group

--- src/app/secure-section/main/balance/balance.component.ts
+++ src/app/secure-section/main/balance/balance.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { MainStorageService } from '../../services/main-storage.service';
+
+import * as fromRoot from '../../../app.reducers';
+import { PaymentMethod } from '../../store/main.model';
 
 @Component({
   selector: 'eaf-balance',
@@ -6,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balance.component.sass']
 })
 export class BalanceComponent implements OnInit {
+  userBalanceState$: Observable<any>;
+  balanceForm: FormGroup;
+  paymentMethod: PaymentMethod;
+  paymentMethods: PaymentMethod[];
 
-  constructor() { }
+  constructor(
+    private store: Store<fromRoot.State>,
+    private mainStorage: MainStorageService,
+  ) { }
 
   ngOnInit() {
+    this.paymentMethods = this.mainStorage.getPaymentMethods();
+    this.initForm();
+    this.onChangePaymentMethod(this.paymentMethods[0]);
+    this.userBalanceState$ = this.store.select(fromRoot.getUserBalanceState);
   }
 
+  initForm() {
+    this.balanceForm = new FormGroup({
+      amount: new FormControl(null, Validators.required),
+      details: new FormControl(''),
+      payment: new FormControl(null),
+      regular: new FormControl()
+    });
+  }
+
+  onChangePaymentMethod(payment: PaymentMethod) {
+    this.paymentMethod = payment;
+    this.balanceForm.patchValue({payment: payment});
+  }
+
+  onSubmit() {
+    console.log(this.balanceForm.value);
+  }
 }
