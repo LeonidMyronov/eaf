fc89ff127170a3ad9cc82251118ec1ed868555a5
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-15 18:18:12 +0300

    add transactions table mockup. add adaptive styles

--- src/app/secure-section/main/balance/balance.component.ts
+++ src/app/secure-section/main/balance/balance.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { MainService } from '../../services/main.service';
 import { MainStorageService } from '../../services/main-storage.service';
 
 import * as fromRoot from '../../../app.reducers';
-import { PaymentMethod } from '../../store/main.model';
+import * as fromMain from '../../store/main.reducer';
+import { PaymentMethod, Transaction } from '../../store/main.model';
 
 export interface TransactionQueryParams {
   fromDate: Date;
@@ -19,16 +21,21 @@ export interface TransactionQueryParams {
   templateUrl: './balance.component.html',
   styleUrls: ['./balance.component.sass']
 })
-export class BalanceComponent implements OnInit {
+export class BalanceComponent implements OnInit, AfterContentChecked {
+  @ViewChild('trtb') trtb: ElementRef;
+  @ViewChild('trtl') trtl: ElementRef;
   userBalanceState$: Observable<any>;
+  transactionsState: Transaction[];
   balanceForm: FormGroup;
   paymentMethod: PaymentMethod;
   paymentMethods: PaymentMethod[];
   transactionQueryParams: TransactionQueryParams;
+  transactionsTableHeads: string[];
 
   constructor(
     private store: Store<fromRoot.State>,
     private mainStorage: MainStorageService,
+    private mainService: MainService,
   ) { }
 
   ngOnInit() {
@@ -37,6 +44,25 @@ export class BalanceComponent implements OnInit {
     this.transactionQueryParams = this.initTransactionQueryParams();
     this.onChangePaymentMethod(this.paymentMethods[0]);
     this.userBalanceState$ = this.store.select(fromRoot.getUserBalanceState);
+    this.store.select(fromMain.getTransactions).subscribe((r: Transaction[]) => {
+      if (!r.length) {
+        this.mainService.fetchTransactionsByPeriod(this.transactionQueryParams);
+      } else {
+        this.createTrTableHeads(r[0]);
+        this.transactionsState = r;
+      }
+    });
+  }
+
+  ngAfterContentChecked() {
+    const trtbArr = this.trtb.nativeElement.children;
+    const trtlArr = this.trtl.nativeElement.children;
+    for (let i = 0; i < trtbArr.length; i++) {
+      trtlArr[i].style.height = trtbArr[i].clientHeight + 'px';
+      // console.log(i, trtbArr[i].clientHeight);
+    }
+    // console.log(trtbArr);
+    // console.log(trtlArr);
   }
 
   initForm() {
@@ -74,5 +100,12 @@ export class BalanceComponent implements OnInit {
     this.transactionQueryParams.toDate = queryParams.toDate;
     this.transactionQueryParams.paymentMethod = queryParams.dropdownItem;
     // console.log(queryParams, this.transactionQueryParams);
+  }
+
+  createTrTableHeads(el: Transaction) {
+    this.transactionsTableHeads = Object.keys(el).slice();
+  }
+  getTrTableHeads() {
+    return [...this.transactionsTableHeads];
   }
 }
