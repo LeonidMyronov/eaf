f27f4df3ca2d535e3cc43e1e370e95f417084a1a
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-18 20:36:07 +0300

    news-list mockup. news-item mockup. adaptive styles

--- src/app/secure-section/main/news/news-item/news-item.component.ts
+++ src/app/secure-section/main/news/news-item/news-item.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
 
+import * as fromRoot from '../../../../app.reducers';
 import { News } from '../../../store/main.model';
+
 
 @Component({
   selector: 'eaf-news-item',
   templateUrl: './news-item.component.html',
   styleUrls: ['./news-item.component.sass']
 })
-export class NewsItemComponent implements OnInit {
+export class NewsItemComponent implements OnInit, OnDestroy {
   @Input() news: News;
-  constructor() { }
+  public displayContent: boolean;
+  private subs: Subscription;
+  constructor(
+    private store: Store<fromRoot.State>
+  ) { }
 
   ngOnInit() {
+    this.subs = this.store.select(fromRoot.getActiveMediaQuery)
+      .subscribe(
+        (activeMedia: string) => this.displayContent = activeMedia === 'xs' ? false : true
+      );
   }
 
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
