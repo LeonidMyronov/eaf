d02896201269a268b7b57f95f40fbc2f70e0579b
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-22 15:05:56 +0300

    add PreloadAllModules mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AuthGuard } from './auth/auth.guard';
 
@@ -16,7 +16,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: false })], // <-- debugging purposes only],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false, // <-- debugging purposes only],
+    // preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule],
   providers: [AuthGuard]
 })
