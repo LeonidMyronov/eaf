03f01094ea61f4de13f5660ab9226561a2314045
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-22 16:23:41 +0300

    output data to template

--- src/app/secure-section/main/discount/discount.component.ts
+++ src/app/secure-section/main/discount/discount.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+
+import { MainService } from '../../services/main.service';
+
+import * as fromMain from '../../store/main.reducer';
+import { DiscountIntro } from '../../store/main.model';
 
 @Component({
   selector: 'eaf-discount',
   templateUrl: './discount.component.html',
   styleUrls: ['./discount.component.sass']
 })
-export class DiscountComponent implements OnInit {
+export class DiscountComponent implements OnInit, OnDestroy {
   discountForm: FormGroup;
-  constructor() { }
+  discountIntroData: DiscountIntro;
+  private subs: Subscription;
+
+  constructor(
+    private store: Store<fromMain.MainState>,
+    private mainService: MainService
+  ) { }
   ngOnInit() {
+    this.subs = this.store.select(fromMain.getDiscountIntro)
+      .subscribe((response: DiscountIntro) => {
+        if (!response.visitorsLastMonth) {
+          this.mainService.fetchDiscountIntro();
+        } else {
+          this.discountIntroData = response;
+        }
+      });
+    this.initForm();
+  }
+
+  initForm() {
     this.discountForm = new FormGroup({
       name: new FormControl('', Validators.required),
       proto: new FormControl('http'),
@@ -22,4 +47,7 @@ export class DiscountComponent implements OnInit {
     console.log(this.discountForm.value);
   }
 
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
