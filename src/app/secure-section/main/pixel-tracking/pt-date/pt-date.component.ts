93a19915f51a0e55d37c2806cb2dccd5a4efed82
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-05-30 12:54:46 +0300

    add pt-stat mockup table

--- src/app/secure-section/main/pixel-tracking/pt-date/pt-date.component.ts
+++ src/app/secure-section/main/pixel-tracking/pt-date/pt-date.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 
 import * as MainActions from '../../../store/main.actions';
 import * as fromMain from '../../../store/main.reducer';
+import { PixelTrackingEvent } from '../../../store/main.model';
 
 @Component({
   selector: 'eaf-pt-date',
@@ -16,8 +17,9 @@ export class PtDateComponent implements OnInit, OnDestroy {
   dateInput: Date;
   currentPTEvent: string;
   panelTitle = 'Pixel Tracking';
+  ptEvents: PixelTrackingEvent[];
   ptEventsNamesState: Observable<string[]>;
-  private subs: Subscription;
+  ptTableHeads: string[];
 
   constructor(
     private router: Router,
@@ -29,25 +31,38 @@ export class PtDateComponent implements OnInit, OnDestroy {
     this.route.params.subscribe(response => {
       this.dateInput = new Date(response.date.replace(/-/g, ','));
     });
+
     this.route.queryParams.subscribe(response => {
       this.currentPTEvent = response.event;
       this.store.dispatch(new MainActions.BeforeFetchPTEventsDetails({date: this.dateInput, eventName: this.currentPTEvent}));
     });
-    this.subs = this.store.select(fromMain.getPTEventsDetails)
-    .subscribe(r => console.log(r));
+
+    this.store.select(fromMain.getPTEventsDetails)
+    .subscribe(r => {
+      this.ptEvents = r;
+      this.createPTTableHeads(this.ptEvents[0]);
+
+    });
 
     this.ptEventsNamesState = this.store.select(fromMain.getPTEventsNames);
 
   }
 
   onChangePanelQuery(e: {date: Date, dropdown: string}) {
-    console.log(e);
     const routeSuffix = `${e.date.getFullYear()}-${e.date.getMonth() + 1}-${e.date.getDate()}`;
     this.router.navigate(['../', routeSuffix], {relativeTo: this.route, queryParams: {event: e.dropdown}});
   }
 
+  createPTTableHeads(el: PixelTrackingEvent) {
+    this.ptTableHeads = Object.keys(el).slice();
+  }
+
+  getPTTableHeads() {
+    return this.ptTableHeads.slice();
+  }
+
   ngOnDestroy() {
-    this.subs.unsubscribe();
+    // this.subs.unsubscribe();
   }
 
 }
