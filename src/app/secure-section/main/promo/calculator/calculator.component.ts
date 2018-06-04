9f0ac0a8035035eb7aa6532c44f0d7fc2c86bbf9
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-06-04 11:39:01 +0300

    create child promo components

---
+++ src/app/secure-section/main/promo/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eaf-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.sass']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
