87490ddc375429ad2bdfc3adc7923f1c9ce9c3bd
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-06-07 12:13:34 +0300

    create base abstract class for banners

---
+++ src/app/secure-section/main/promo/base-banner/base-banner.component.ts
@@ -0,0 +1,47 @@
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { MainService } from '../../../services/main.service';
+
+import * as fromMain from '../../../store/main.reducer';
+import { Banner } from '../promo.model';
+import { Coupon } from '../../../store/main.model';
+
+export abstract class BaseBannerComponent {
+  public promoState$: Observable<{siteName: string, refLink: string, banners: Banner[], coupons: Coupon[]}>;
+
+  constructor(
+    protected store: Store<fromMain.State>,
+    protected mainService: MainService
+  ) {}
+
+  abstract onSelectCoupon(id: number, coupon: Coupon): void;
+
+  abstract onAddUtm(id: number, utm: string): void;
+
+  generateSnippet(siteName: string, refLink: string, banner: Banner): string {
+    let snippet = `<!--Start ${siteName} code-->
+    `;
+    let updatedRefLink = '<a href="' + refLink;
+      if (banner.coupon) {
+        updatedRefLink += '&coupon=' + banner.coupon;
+      }
+      if (banner.utm) {
+        updatedRefLink += '&utm=' + encodeURIComponent(banner.utm);
+      }
+    snippet += updatedRefLink + '"> ';
+    snippet += `
+      <img src="${banner.bannerSrc}" alt="${banner.title}" title="${banner.title}"/>
+      </a>
+      <!--End ${siteName} code-->`;
+
+    return snippet;
+  }
+
+  onCopyToClipboard(value) {
+    this.mainService.copyToClipboard(value)
+      .then(resolve => console.log(`Text ${resolve} copied successefully`))
+      .catch(error => console.log(`Error ${error} occured while copying text`));
+  }
+
+}
