e3ce661d7eeabca1b265b539aecdfc20628de7a0
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-06-12 11:03:23 +0300

    calc sibe-bar mockup

--- src/app/secure-section/main/promo/calculator/calculator.component.ts
+++ src/app/secure-section/main/promo/calculator/calculator.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MainStorageService } from '../../../services/main-storage.service';
+import { MainService } from '../../../services/main.service';
+
+import { PromoCalcView, PromoCalcColorScheme } from '../../../store/main.model';
+
 @Component({
   selector: 'eaf-calculator',
   templateUrl: './calculator.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  public calcViews: PromoCalcView[];
+  public selectedCalcView: PromoCalcView;
+  public calcColSchs: PromoCalcColorScheme[];
+  public selectedcCalcColSch: PromoCalcColorScheme;
+  constructor(
+    private mainStorage: MainStorageService,
+    private mainService: MainService
+  ) { }
 
   ngOnInit() {
+    this.calcViews = this.mainStorage.getPromoCalcViews();
+    this.selectedCalcView = this.calcViews[2];
+    this.calcColSchs = this.mainStorage.getPromoCalcColorSchemes();
+    this.selectedcCalcColSch = this.calcColSchs[1];
+  }
+
+  onSelectView(calcView: PromoCalcView) {
+    this.selectedCalcView = calcView;
+  }
+
+  onSelectColSch(colSch: PromoCalcColorScheme) {
+    this.selectedcCalcColSch = colSch;
+  }
+
+  generateSnippet() {}
+
+  onCopyToClipboard(value) {
+    this.mainService.copyToClipboard(value)
+      .then(resolve => console.log(`Text ${resolve} copied successefully`))
+      .catch(error => console.log(`Error ${error} occured while copying text`));
   }
 
 }
