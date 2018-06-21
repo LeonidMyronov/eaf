efee30d9d4d797b2b3c0b4e6c578f2b69af1c46d
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-06-21 22:26:41 +0300

    base notification implementqtion

--- src/app/ui/notification/notification.component.ts
+++ src/app/ui/notification/notification.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'eaf-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.sass']
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent {
   @Input() message;
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
