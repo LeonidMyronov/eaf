ef5f3150a1804bfdb926a5111ea34732f6039a41
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-07-03 13:03:10 +0300

    add component for postback page

--- src/app/secure-section/main.module.ts
+++ src/app/secure-section/main.module.ts
@@ -27,6 +27,8 @@ import { ProfileComponent } from './main/profile/profile.component';
 import { PostbackComponent } from './main/postback/postback.component';
 import { WhitelabelComponent } from './main/whitelabel/whitelabel.component';
 import { OfferComponent } from './main/offer/offer.component';
+import { PtListComponent } from './main/pixel-tracking/pt-list/pt-list.component';
+import { PtFormComponent } from './main/pixel-tracking/pt-form/pt-form.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +47,8 @@ import { OfferComponent } from './main/offer/offer.component';
     PostbackComponent,
     WhitelabelComponent,
     OfferComponent,
+    PtListComponent,
+    PtFormComponent,
   ],
   imports: [
     CoreModule,
--- src/app/secure-section/main/postback/postback.component.ts
+++ src/app/secure-section/main/postback/postback.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from '../../../app.reducers';
+import { Site } from '../../../core/core.model';
 
 @Component({
   selector: 'eaf-postback',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostbackComponent implements OnInit {
 
-  constructor() { }
+  public addPostbackMode: boolean = false;
+  public userSitesState$: Observable<Site[]>;
+  constructor(
+    private store: Store <fromRoot.State>
+  ) { }
 
   ngOnInit() {
+    this.userSitesState$ = this.store.select(fromRoot.getOurSites).map(s => s.filter(e => e.pixelTrackingEnabled));
   }
 
+  onCloseForm($event) {
+    this.addPostbackMode = false;
+  }
+
+  onAddPostback() {
+    this.addPostbackMode = true;
+  }
 }
