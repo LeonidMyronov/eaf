c3d0c5e6359761b199f360388c39e7a360ba2812
LeonidMyronov <leonid.v.mironov@gmail.com>
2018-07-03 13:04:30 +0300

    add component for selecting sites for postback adjustment

---
+++ src/app/secure-section/main/pixel-tracking/pt-form/pt-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eaf-pt-form',
+  templateUrl: './pt-form.component.html',
+  styleUrls: ['./pt-form.component.sass']
+})
+export class PtFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
